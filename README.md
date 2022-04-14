521eae1602b4610e14e323e30318b2cda6e95646
maomao <77312370+sukki37@users.noreply.github.com>
2022-04-14 13:44:16 +0800

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# first-contribution-check-action
-Check if the author of the last commit is a new contributor
+# First Contribution Check Action
+
+This action checks if the author of the last commit is a new contributor of a repository.
+
+## Usage
+### Pre-requisites
+Create a workflow .yml file in your repositories .github/workflows directory. 
+### Outputs
+
+#### `isNewContributor`
+
+flag for filtering merged pull requests from first-time contributors.
+
+### Env
+
+### `GITHUB_TOKEN`
+
+A message template of the form `message message {}`
+
+## Getting Started
+If you're new to actions, add these to your .github/workflows/main.yml file. If this file does not exist, create one.
+```yml
+on:
+  push:
+    branches:
+        - main
+
+jobs:
+  contrib-readme-job:
+    runs-on: ubuntu-latest
+    name: A job to find if a commitor is new to contribute.
+    steps:
+      - name: First Contribution Check
+        uses: sukki37/first-contribution-check-action@v1.0
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+      - name: Print Output of Previous Step
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+That's it!
+
+To add it to your to your existing workflow, append this to your current `.yml` workflow script.
+
+```yml
+uses: sukki37/first-contribution-check-action@v1.0
+env:
+  GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ src/index.js
@@ -0,0 +1,83 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const accessToken = process.env['GITHUB_TOKEN'];
+async function isFirstContribution(
+  client, owner, repo,
+  sender, 
+  page = 1
+) {
+  const { status, data: contributors } = await client.repos.listContributors({
+    owner: owner,
+    repo: repo,
+    per_page: 100,
+    page: page,
+    state: 'all'
+  });
+
+  if (status !== 200) {
+    throw new Error(`Received unexpected API status code ${status}`);
+  }
+
+  if (contributors.length === 0) {
+    return true;
+  }
+
+  for (const contributor of contributors) {
+    core.info(JSON.stringify(contributor))
+    const login = contributor.login;
+    if (login === sender) {
+      core.info("===================")
+      core.info(login)
+      core.info("===================")
+      return false;
+    }
+  }
+
+  return await isFirstContribution(
+    client,
+    owner,
+    repo,
+    sender,
+    page + 1
+  );
+}
+
+
+async function run() {
+  try {
+    const payload = github.context.payload;
+    const githubClient = github.getOctokit(accessToken);
+    core.info("Request received");
+
+    if (payload.event_name === "push") {
+      core.info("Checking begins...");
+      const userName = payload.event.commits[0].author.name;
+      const owner = payload.repository_owner;
+      const repoName = payload.event.repository.name;
+
+      const isFirst = await isFirstContribution(
+        githubClient, owner, repoName,
+        userName
+      );
+      
+      if (isFirst) {
+        core.info("First Successful PR");;
+      }else {
+        core.info("Not First Successful PR");
+      }
+      core.setOutput('is_new_contributor', isFirst);
+    }else {
+      core.warning("event type [" + payload.event_name +"] is unsupported now")
+    }
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+
+}
+
+run();
+
+
+
+
+
